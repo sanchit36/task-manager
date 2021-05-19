f5a6aeb67275682f7f72b239632b737d13d1e75a
snachit36 <sbhadgal_be19@thapar.edu>
2021-05-19 22:11:21 +0530

    react-create-app on Heroku

--- src/pages/TaskDetail.js
+++ src/pages/TaskDetail.js
@@ -32,10 +32,6 @@ const TaskDetail = () => {
                   ...comment.data(),
                 }))
               );
-            })
-            .catch((error) => {
-              setLoading(false);
-              console.log(error);
             });
         })
         .catch((error) => {
@@ -64,8 +60,7 @@ const TaskDetail = () => {
 
           {task.fileType === "video" && (
             <video width="320" height="240" controls>
-              <source src="movie.mp4" type="video/mp4" />
-              <source src="movie.ogg" type="video/ogg" />
+              <source src={task.fileURL} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           )}
